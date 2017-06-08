88a99d48619b1e4c5d32bee220921453ec8d7eeb
Rune <rune@Runes-iMac.local>
2017-06-08 13:41:11 +0200

    Reference to oauth2

--- src/auth/oauth2/auth.token.schemas.ts
+++ src/auth/oauth2/auth.token.schemas.ts
@@ -1,5 +1,7 @@
 import { setType, lib } from '@ords/obj-schema';
 
+// http://scottksmith.com/blog/2014/07/02/beer-locker-building-a-restful-api-with-node-oauth2-server/
+
 let requiredString: lib.SingleElementDefinition = {
     numValues: 1,
     maxNumValues: 1,
